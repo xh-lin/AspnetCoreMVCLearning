6d4aba27854314424cd8a7e60a6153953eb6f88d
xh-lin <xuhuanglin99@gmail.com>
2023-07-30 14:14:37 -0400

    seed data for Category table and data and display data on UI

--- WebApp/Controllers/CategoryController.cs
+++ WebApp/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }

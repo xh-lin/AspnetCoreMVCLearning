43294d11e0765cf71547c0a69869a781f6746598
xh-lin <xuhuanglin99@gmail.com>
2023-07-30 17:47:09 -0400

    create Category form UI and save new Category into DB

--- WebApp/Controllers/CategoryController.cs
+++ WebApp/Controllers/CategoryController.cs
@@ -18,5 +18,18 @@ namespace WebApp.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- WebApp/Models/Category.cs
+++ WebApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models
 {
@@ -6,8 +7,12 @@ namespace WebApp.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }

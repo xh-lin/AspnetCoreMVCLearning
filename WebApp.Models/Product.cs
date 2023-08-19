06bbedfefe6891f78bfdd9cb2c7b591a8d285c62
xh-lin <xuhuanglin99@gmail.com>
2023-08-19 10:20:00 -0400

    Add foreign key in EF Core for product and category

--- WebApp.Models/Product.cs
+++ WebApp.Models/Product.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp.Models
 {
@@ -43,5 +44,10 @@ namespace WebApp.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }

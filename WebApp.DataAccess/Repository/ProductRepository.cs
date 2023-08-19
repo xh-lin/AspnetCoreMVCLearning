1d230207246e2869e1fc63b2c42473cffe562d21
xh-lin <xuhuanglin99@gmail.com>
2023-08-19 14:25:58 -0400

    optional custom update code for Product

--- WebApp.DataAccess/Repository/ProductRepository.cs
+++ WebApp.DataAccess/Repository/ProductRepository.cs
@@ -17,7 +17,23 @@ namespace WebApp.DataAccess.Repository
 
         public void Update(Product obj)
         {
-            _db.Products.Update(obj);
+            Product? objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Author = obj.Author;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.CategoryId = obj.CategoryId;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
         }
     }
 }

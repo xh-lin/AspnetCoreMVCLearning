5519aaa8affcd53bd98da522cb55849dd1f266f4
xh-lin <xuhuanglin99@gmail.com>
2023-08-19 09:19:54 -0400

    create Product model, seed data, repository and UnitOfWork

---
+++ WebApp.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.DataAccess.Data;
+using WebApp.DataAccess.Repository.IRepository;
+using WebApp.Models;
+
+namespace WebApp.DataAccess.Repository
+{
+    internal class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}

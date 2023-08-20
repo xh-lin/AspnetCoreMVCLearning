bd43fc89d1a0339adc75321bda7da2f2e9956bef
xh-lin <xuhuanglin99@gmail.com>
2023-08-20 11:38:16 -0400

    display Products on the home page

--- WebApp/Areas/Customer/Controllers/HomeController.cs
+++ WebApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebApp.DataAccess.Repository;
+using WebApp.DataAccess.Repository.IRepository;
 using WebApp.Models;
 
 namespace WebApp.Areas.Customer.Controllers
@@ -8,15 +10,18 @@ namespace WebApp.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productlist = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productlist);
         }
 
         public IActionResult Privacy()

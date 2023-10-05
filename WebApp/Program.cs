68e1085ec20cd6d5efa417e03897eefd27a04707
xh-lin <xuhuanglin99@gmail.com>
2023-10-05 18:19:19 -0400

    configure Razor Pages to fix login link not working

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options=>
 
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.AddRazorPages();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -36,6 +38,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");

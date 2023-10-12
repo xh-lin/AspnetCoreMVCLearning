49e160f8c35e30b21e6ebcdbdd03ba16f9982786
xh-lin <xuhuanglin99@gmail.com>
2023-10-12 19:09:41 -0400

    add ApplicationUser to repository

--- WebApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ WebApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApp.Models;
 
 namespace WebApp.DataAccess.Repository.IRepository
 {
     public interface IUnitOfWork
     {
+        IApplicationUserRepository ApplicationUser { get; }
         ICategoryRepository Category { get; }
         ICompanyRepository Company { get; }
         IProductRepository Product { get; }
--- WebApp.DataAccess/Repository/UnitOfWork.cs
+++ WebApp.DataAccess/Repository/UnitOfWork.cs
@@ -13,6 +13,7 @@ namespace WebApp.DataAccess.Repository
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _db;
+        public IApplicationUserRepository ApplicationUser { get; private set; }
         public ICategoryRepository Category { get; private set; }
         public ICompanyRepository Company { get; private set; }
         public IProductRepository Product { get; private set; }
@@ -21,10 +22,11 @@ namespace WebApp.DataAccess.Repository
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
-            Category = new CategoryRepository(_db);
-            Product = new ProductRepository(_db);
-            Company = new CompanyRepository(_db);
-            ShoppingCart = new ShoppingCartRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(db);
+            Category = new CategoryRepository(db);
+            Product = new ProductRepository(db);
+            Company = new CompanyRepository(db);
+            ShoppingCart = new ShoppingCartRepository(db);
         }
 
         public void Save()

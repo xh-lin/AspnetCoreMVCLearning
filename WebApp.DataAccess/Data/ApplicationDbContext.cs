30bc724097e51b61ea44b496fd88c6289fe732ef
xh-lin <xuhuanglin99@gmail.com>
2023-10-12 18:47:28 -0400

    add ShoppingCart model

--- WebApp.DataAccess/Data/ApplicationDbContext.cs
+++ WebApp.DataAccess/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace WebApp.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)

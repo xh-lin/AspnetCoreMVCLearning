b2847d95c6a5f65780f3c72429c9e72328a48802
xh-lin <xuhuanglin99@gmail.com>
2023-10-13 21:16:08 -0400

    order Summary UI

--- WebApp/Areas/Customer/Controllers/CartController.cs
+++ WebApp/Areas/Customer/Controllers/CartController.cs
@@ -40,6 +40,11 @@ namespace WebApp.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
         public IActionResult Plus(int cartId) 
         {
             var cartromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);

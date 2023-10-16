aed4c62d0bf05e34d22a6bdd13e0cb4a212890ea
xh-lin <xuhuanglin99@gmail.com>
2023-10-16 13:33:39 -0400

    OrderVM and OrderController

---
+++ WebApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.DataAccess.Repository.IRepository;
+using WebApp.Models;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: nameof(ApplicationUser)).ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+
+		#endregion
+	}
+}

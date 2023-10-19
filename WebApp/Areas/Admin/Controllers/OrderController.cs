a33071e6f9840a18269230adde41efefceadbf5f
xh-lin <xuhuanglin99@gmail.com>
2023-10-19 19:48:58 -0400

    payment status filter in Order List

--- WebApp/Areas/Admin/Controllers/OrderController.cs
+++ WebApp/Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using WebApp.DataAccess.Repository.IRepository;
 using WebApp.Models;
+using WebApp.Utility;
 
 namespace WebApp.Areas.Admin.Controllers
 {
@@ -24,10 +26,27 @@ namespace WebApp.Areas.Admin.Controllers
 		#region API CALLS
 
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: nameof(ApplicationUser)).ToList();
-			return Json(new { data = objOrderHeaders });
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: nameof(ApplicationUser)).ToList();
+
+            switch (status)
+            {
+                case "inprocess":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusInProcess);
+                    break;
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusApproved);
+                    break;
+            }
+
+            return Json(new { data = objOrderHeaders });
 		}
 
 		#endregion

41a6cbdec0c90860c6b733876429c722831e8c03
xh-lin <xuhuanglin99@gmail.com>
2023-10-21 18:49:08 -0400

    ship order

--- WebApp.DataAccess/Repository/OrderHeaderRepository.cs
+++ WebApp.DataAccess/Repository/OrderHeaderRepository.cs
@@ -27,7 +27,7 @@ namespace WebApp.DataAccess.Repository
 			if (orderFromDb != null)
 			{
 				orderFromDb.OrderStatus = orderStatus;
-				if (string.IsNullOrEmpty(paymentStatus))
+				if (!string.IsNullOrEmpty(paymentStatus))
 				{
 					orderFromDb.PaymentStatus = paymentStatus;
 				}

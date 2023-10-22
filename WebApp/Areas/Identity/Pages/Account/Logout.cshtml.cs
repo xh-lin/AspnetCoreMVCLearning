564a8c9ad4241955c8ab3616e8336f7cdf54ca0c
xh-lin <xuhuanglin99@gmail.com>
2023-10-22 14:18:05 -0400

    clear session when log out

--- WebApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ WebApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -26,6 +26,9 @@ namespace WebApp.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
+
+            HttpContext.Session.Clear();
+
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {

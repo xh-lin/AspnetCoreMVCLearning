94b967d099e69d9216053dfbea3c98fea9868fea
xh-lin <xuhuanglin99@gmail.com>
2023-10-22 15:37:58 -0400

    creating Admin and Employee account from the Admin side

--- WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -212,7 +212,14 @@ namespace WebApp.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        } 
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }

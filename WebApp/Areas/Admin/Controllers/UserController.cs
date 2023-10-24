08759842b9ce1c4ac113a42027248ce984b3fc9a
xh-lin <xuhuanglin99@gmail.com>
2023-10-24 18:04:23 -0400

    display User list

---
+++ WebApp/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using WebApp.DataAccess.Data;
+using WebApp.DataAccess.Repository;
+using WebApp.DataAccess.Repository.IRepository;
+using WebApp.Models;
+using WebApp.Models.ViewModels;
+using WebApp.Utility;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+
+
+            return Json(new { data = objUserList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
---
+++ WebApp/wwwroot/js/user.js
@@ -0,0 +1,27 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/User/GetAll' },
+        "columns": [
+            { data: 'name', "width": "15%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'company.name', "width": "15%" },
+            { data: null, "width": "15%" },
+            {
+                data: 'id',
+                "render": function (id) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/Admin/User/Upsert?id=${id}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}

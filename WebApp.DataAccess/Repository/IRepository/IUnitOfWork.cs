bca85a237c489a4dccd6b15545ab02ae58557095
xh-lin <xuhuanglin99@gmail.com>
2023-10-27 14:37:03 -0400

    add ProductImageRepository

--- WebApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ WebApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -16,6 +16,7 @@ namespace WebApp.DataAccess.Repository.IRepository
         IShoppingCartRepository ShoppingCart { get; }
         IOrderHeaderRepository OrderHeader { get; }
         IOrderDetailRepository OrderDetail { get; }
+        IProductImageRepository ProductImage { get; }
 
         void Save();
     }
---
+++ WebApp.DataAccess/Repository/ProductImageRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.DataAccess.Data;
+using WebApp.DataAccess.Repository.IRepository;
+using WebApp.Models;
+
+namespace WebApp.DataAccess.Repository
+{
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository
+    {
+        public ProductImageRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public void Update(ProductImage obj)
+        {
+            _db.ProductImages.Update(obj);
+        }
+    }
+}

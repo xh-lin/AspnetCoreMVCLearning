853f6f2ce8179d4db227ce15f7944755771004bf
xh-lin <xuhuanglin99@gmail.com>
2023-10-27 16:07:10 -0400

    display image on home page and in shopping cart

--- WebApp/Areas/Customer/Controllers/CartController.cs
+++ WebApp/Areas/Customer/Controllers/CartController.cs
@@ -35,8 +35,11 @@ namespace WebApp.Areas.Customer.Controllers
                 OrderHeader = new()
             };
 
+            IEnumerable<ProductImage> productImages = _unitOfWork.ProductImage.GetAll();
+
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
+                cart.Product.ProductImages = productImages.Where(u => u.ProductId == cart.Product.Id).ToList();
                 cart.Price = GetPriceBasedOnQuatity(cart);
                 ShoppingCartVM.OrderHeader.OrderTotal += cart.Price * cart.Count;
             }
